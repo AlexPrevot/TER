d829d919e92b04a50ca2a347c1a767f3830567b5
Alexandre Prevot <alexandrephilipe.prevot@gmail.com>
2022-03-27 12:16:48 +0200

    Updated Path class

--- path.cpp
+++ path.cpp
@@ -1,1 +1,32 @@
 #include "path.h"
+
+//================================= PUBLIC =====================
+int Path::getSize()
+{
+	return _path.size();
+}
+
+int Path::getFitness()
+{
+	return _fitness;
+}
+
+
+
+//================================= PRIVATE =====================
+
+void Path::_setFitness(float fitness)
+{
+	_fitness = fitness;
+}
+
+void Path::_computeFitness()
+{
+	float length = 0;
+	for (int i = 2; i < _path.size(); i += 2)
+	{
+		length += abs(_path.at(i) - _path.at(i - 2)) + abs(_path.at(i + 1) - _path.at(i - 1));
+	}
+
+	_setFitness(length);
+}
--- path.h
+++ path.h
@@ -7,32 +7,38 @@
 A path is a 1-D vector of coordinates. Each coordinate are paired in two
 The path does operation similar as if it was a 2D vector.
 
-
+Everytime the path is updated, the fitness is too.
 
 (!) IMPORTANT : The fitness is not the overall length of the path. 
 It only is the sum of differences between contiguous coordinates. 
 That way, square roots are avoided.
 
 */
+
+typedef std::vector<int> coords;
+
 class Path
 {
 	
 	public:
-		Path(int size)
-		{
-			_path = std::vector<int>(size, -1);
-		}
-
 		Path(std::vector<int> path)
 		{
 			_path = path;
+			_computeFitness();
 		}
 
+		//---- getters
+		int		getSize();
+		int		getFitness();
 
+		
 	private:
 
-		std::vector<int> _path;
-		float fitness = -1;
+		void _setFitness(float fitness);
+		void _computeFitness();
+
+		coords _path;
+		int _fitness = -1;
 
 };
 
