f5ffdeb0699b5cfd4aed70746bb130774f01c28d
Alexandre Prevot <alexandrephilipe.prevot@gmail.com>
2022-03-30 21:31:00 +0200

    modify crosser

--- crosser.cpp
+++ crosser.cpp
@@ -8,31 +8,31 @@
 
 Path Crosser::cross(Path & path1, Path& path2)
 {
+	using namespace std;
+	int size = path1.getSize();
 
-	int node = rand() % path1.getSize();
-
-	std::unordered_map<int, int> path2_rep;
-
-	for (int i = 0; i < path2.getSize(); i++)
-		path2_rep.insert({ path2.at(i), i });
-
-
-	std::unordered_map<int, int> vec_rep; //representq the ouput vector in an unordered_map
-
-
-	std::vector<int> ans_path(path1.getSize());
-
-	for(int i = 0; i < path1.getSize(); i++)
-	{
-		int elem = path1.at(node);
-		vec_rep.insert({ elem, i });
-
-
-		
-
-
-	}
+	int node = rand() % size;
 	
+	vector<int> vec1(size, -1);
+	vector<int> vec2(size, -1);
+
+	//initialize 1st vector
+	for (int i = 0; i < size - 1; i++)
+		vec1[path1.at(i)] = path1.at(i + 1);
+	vec1[path1.at(size - 1)] = path1.at(0);
+
+	//initialize 2nd vector
+	for (int i = 0; i < size - 1; i++)
+		vec2[path2.at(i)] = path2.at(i + 1);
+	vec2[path2.at(size - 1)] = path2.at(0);
+
+
+	
+
+
+	vector<int> ans_rep(size, -1);
+
+
 
 	Path ans({ 1,2,3 });
 	return ans;
